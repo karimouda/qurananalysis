3a574e95c2002272b7b28d40b0cb0f95710b74f2
karimouda <karim@ouda.net>
2015-07-15 22:53:23 +0100

    concordance tool - alhamd lelah

---
+++ analysis/word-context-concordance.php
@@ -0,0 +1,192 @@
+<?php 
+require_once("../global.settings.php");
+
+$lang = "AR";
+
+
+
+if ( isset($_GET['lang']) )
+{
+	$lang = $_GET['lang'];
+}
+
+loadModels("core",$lang);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Quran Analysis | Words Context ( Concordance ) </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Information for each word in the Quran">
+    <meta name="author" content="">
+
+	<script type="text/javascript" src="<?=$JQUERY_PATH?>" ></script>
+	<script type="text/javascript" src="<?=$MAIN_JS_PATH?>"></script>
+	<link rel="stylesheet" href="/qe.style.css?bv=<?=$BUILD_VERSION?>" />
+	 
+	  
+	 
+	<script type="text/javascript">
+	</script>
+     
+       
+  </head>
+  <body>
+
+	<?php 
+		require_once("./analysis.header.php");
+	?>
+				
+  <div id='main-container'>
+			  	
+ 				<h1 class='analysis-title-header'>Concordance</h1>
+			  	
+			  	
+			  	<div id='words-context-options'>
+					Word or PoS Tag:
+					<input type="text" id="word" autofocus="true" />
+					<br>
+					Number of words around
+					<input type="text" id="level" style="width:20px" maxlength="1" placeholder="3" />
+					<input type="button" id='words-context-submit' value='Concordance'  />
+				</div>
+				
+				<div id='words-context-area'>
+				<?php 
+				
+				/*
+				$str = "Leeds is a university";
+				$strArr = preg_split("/ /", $str);
+				$strWordsCount = count($strArr);
+				
+				$ssLength  = $strWordsCount;
+				
+				while($ssLength>1)
+				{
+					$groupLoopsNeeded= (($strWordsCount-$ssLength)+1);
+					
+					$ssStr="";
+					$v=0;
+					while($v<$groupLoopsNeeded)
+					{
+						$ssStr= join(" ",array_slice($strArr, $v,$ssLength));
+						echoN($ssStr);
+						$v++;
+					}
+					
+					$ssLength--;
+				}
+				exit;
+				*/
+				
+				?>
+				</div>
+					<div id="loading-layer">
+			  		Loading ...
+					</div>
+		  		
+			
+   </div>
+ 
+
+
+	<script type="text/javascript">
+
+
+				
+		$(document).ready(function()
+		{
+
+
+		
+		});
+
+
+		$("#words-context-submit").click(function()
+		{
+	        var word = $("#word").val();
+
+
+			if ( word=="" )
+			{
+				return;
+			}
+
+			var level = $("#level").val();
+
+
+
+			$("#loading-layer").show();
+
+			getWordContext(word,level);
+
+				  
+		});
+
+		
+		function getWordContext(word,level)
+		{
+		
+
+	
+				$("#words-context-area").html("");
+				
+				$.ajaxSetup({
+					url:  "/analysis/wordcontext-concordance.ajax.service.php?lang=<?=$lang?>&word="+encodeURIComponent(word)+"&level="+level,
+					global: false,
+					type: "GET"
+					
+				  });
+
+
+				$.ajax({
+					
+					timeout: 60000,
+					success: function(retRes)
+							{
+
+						  			$("#loading-layer").hide();
+						      	 	
+						  			$("#words-context-area").html(retRes);
+
+						  	
+	
+						 	 	
+						     },
+					      	 error: function (xhr, ajaxOptions, thrownError)
+					         {
+					      		$("#words-context-area-area").html("<center>Error occured !</center>");
+					      		$("#loading-layer").hide();
+					         }
+						});
+							
+				
+				
+				
+				
+				
+		}
+
+
+
+
+
+
+	</script>
+		
+
+
+	
+
+
+  </body>
+</html>
+
+
+
+
+
+
+
---
+++ analysis/wordcontext-concordance.ajax.service.php
@@ -0,0 +1,439 @@
+<?php 
+require_once("../global.settings.php");
+
+$lang = "AR";
+
+
+
+if ( isset($_GET['lang']) )
+{
+	$lang = $_GET['lang'];
+}
+
+loadModels("core,search,qac",$lang);
+
+
+
+$word = trim($_GET['word']);
+$userPreferedContextLevel =  trim($_GET['level']);
+
+
+
+
+	
+//preprint_r($posTaggedSubsentences);
+
+//echoN("SubSentences Count:".addCommasToNumber(count($posTaggedSubsentences)));
+
+$topPoSAggregation = array();
+$ssPoSAggregation = array();
+$ssPoSAggregationCorrespondingSent = array();
+
+
+
+			function getContextPhrases($contextArr,$maxLevel,$beforeOrAfter="BEFORE")
+			{
+				$phraseStr = "";
+				
+				$levelSign = "-";
+				
+				if ($beforeOrAfter!="BEFORE")
+				{
+					// then after
+					$levelSign = "+";
+				}
+				
+				
+					
+				if ( $levelSign=="+")
+				{
+					for($i=1;$i<=$maxLevel;$i++)
+					{
+						// ["+1i]
+						foreach($contextArr["$levelSign"."$i"] as $word => $freq)
+						{
+							if ( empty($word)) continue;
+							
+							$phraseStr = $phraseStr. " ".$word;
+						}
+					}
+				}
+				else
+				{
+					for($i=$maxLevel;$i>=1;$i--)
+					{
+						// ["+1i]
+						foreach($contextArr["$levelSign"."$i"] as $word => $freq)
+						{
+							if ( empty($word)) continue;
+								
+							$phraseStr = $phraseStr. " ".$word;
+						}
+					}
+				}
+					
+				
+				return trim($phraseStr);
+				
+				
+			}
+		
+			//echoN("Word:$word");
+			
+
+			$targetType = "POS";
+			
+			if ( isArabicString($word))
+			{
+				$targetType = "WORD";
+				
+				if ( isSimpleQuranWord($word))
+				{
+					$posTaggedSubsentences = getPoSTaggedSubsentences("SIMPLE");
+				}
+				else
+				{
+					$posTaggedSubsentences = getPoSTaggedSubsentences();
+				}
+			}
+			else
+			{
+				$posTaggedSubsentences = getPoSTaggedSubsentences();
+			}
+			
+			$targetPOSorWord = trim($word);
+			
+			
+			if ( $targetType=="POS" )
+			{
+				if (   !isset($MODEL_QAC['QAC_POS'][$targetPOSorWord]))
+				{
+					showTechnicalError("Not a valid PoS tag !");
+					exit;
+				}
+			}
+			else 
+			{
+				if (  empty($targetPOSorWord) )
+				{
+					showTechnicalError("Word not valid !");
+					exit;
+				}
+			}
+			
+			
+			/////////// PREPARE CONTEXT ARRAY ////////////////
+			$concordanceMaxLevel = 3;
+			
+			if ( !empty($userPreferedContextLevel)) 
+			{
+				$concordanceMaxLevel = $userPreferedContextLevel;
+				
+			}
+			
+	
+			
+			for($i=$concordanceMaxLevel;$i>=1;$i--)
+			{
+				$contextArr["+$i"]=array();
+				
+			}
+			for($i=1;$i<=$concordanceMaxLevel;$i++)
+			{
+				$contextArr["-$i"]=array();
+			}	
+			/////////////////////////////////////////////////
+	
+			//preprint_r($contextArr);exit;
+			$targetWordCounter = 0;
+			
+			$phrasesBeforeArr = array();
+			$phrasesAfterArr = array();
+			$fullSentences = array();
+			$concordanceIndex=0;
+			
+
+			
+			
+			foreach ($posTaggedSubsentences as $ssLoc => $ssArray)
+			{
+
+				
+				$wordsArr = $ssArray['WORDS'];
+				$posArr = $ssArray['POS_TAGS'];
+				$qacIndexesArr = $ssArray['QAC_WORD_INDEXES'];
+				
+				$wordsOrPoSStr = "";
+				$posOrWordsArr = array();
+				
+				if ($targetType =="POS")
+				{
+					$posOrWordsArr = $posArr;
+					$wordsOrPoSStr = join(" ",$ssArray['POS_TAGS']);
+				}
+				else
+				{
+					$posOrWordsArr = $wordsArr;
+					$wordsOrPoSStr = join(" ",$ssArray['WORDS']);
+				}
+				
+		
+				
+			
+				$posOrWordEntryKey = false;
+				$posOrWordsIndexesArr = array();
+				
+					// didn't use array_search because of "multple tags phrases" 
+					// example searching fro PN while the word tag is "P PN" which will not match
+					foreach($posOrWordsArr as $index => $posTagOrPhrase)
+					{
+						
+						// can't use strpos PN is found in 'IMPN' 
+						if ( preg_match("/(^|[ ])+$targetPOSorWord([ ]|\$)+/", $posTagOrPhrase))
+						{
+							$posOrWordEntryKey = $index;
+							$posOrWordsIndexesArr[]=$index;
+							
+						}
+					}
+					
+			
+				
+				
+				if ( !empty($posOrWordsIndexesArr) )
+				{
+		
+					
+					
+		
+					foreach($posOrWordsIndexesArr as $sequenctialIndex => $wordIndexInArray )
+					{
+						//reset context array
+						$contextArr = array();
+						
+						//$qacWordIndex = $qacIndexesArr[$wordIndexInArray];
+						//echoN(substr($ssLoc,0,strrpos($ssLoc, "-")).":".$qacWordIndex);
+						$targetWordCounter++;
+						
+						if ( $wordIndexInArray>=1)
+						{
+							$level = 1;
+							for($before=$wordIndexInArray-1;$before>=0;$before--)
+							{
+								
+								if ( $level > $concordanceMaxLevel)
+								{
+									break;
+								}
+								
+								$posOrWordInLevel = $posOrWordsArr[$before];
+								$contextArr["-$level"][$posOrWordInLevel]++;
+								
+								$level++;
+								
+								
+							}
+						}
+						
+						$posOrWordArrLength = count($posOrWordsArr);
+						
+						if ( ($posOrWordArrLength-($wordIndexInArray+1))>=1)
+						{
+							$level = 1;
+							for($after=$wordIndexInArray+1;$after<$posOrWordArrLength;$after++)
+							{
+								if ( $level > $concordanceMaxLevel)
+								{
+									break;
+								}
+								
+								$posOrWordInLevel = $posOrWordsArr[$after];
+								$contextArr["+$level"][$posOrWordInLevel]++;
+								$level++;
+							}
+						}
+					}
+					
+					
+					$fullSentences[$concordanceIndex]="";
+					$phraseBefore = getContextPhrases($contextArr,$concordanceMaxLevel,"BEFORE");
+					$phraseAfter = getContextPhrases($contextArr,$concordanceMaxLevel,"AFTER");
+					
+					if ( !empty($phraseBefore))
+					{
+						$phrasesBeforeArr[$phraseBefore]++;
+						
+						$fullSentences[$concordanceIndex]=$phraseBefore;
+					}
+					
+					$fullSentences[$concordanceIndex] .= " $targetPOSorWord ";
+					
+					if ( !empty($phraseAfter))
+					{
+						$phrasesAfterArr[$phraseAfter]++;
+						
+						$fullSentences[$concordanceIndex] .=$phraseAfter;
+					}
+					
+					
+					$concordanceIndex++;
+					
+					
+
+				}
+				
+				
+				//preprint_r($contextArr);
+				
+				
+				//echoN($phraseBefore);
+				//echoN($phraseAfter);
+				
+				
+			
+				
+				//preprint_r($phrasesAfterArr);
+				
+				
+		
+				
+			}
+			
+		
+			arsort($phrasesBeforeArr);
+			arsort($phrasesAfterArr);
+			
+			
+			//preprint_r($fullSentences);
+			
+			//preprint_r($phrasesBeforeArr);
+			//preprint_r($phrasesAfterArr);
+			
+			
+		
+	
+		
+		
+		if ( count($fullSentences)==0)
+		{
+			showTechnicalError("No data found, make sure the word is found in the Quran");
+			showTechnicalError("Use proper Simple or Uthmani word ");
+			showTechnicalError("Use Word information tool to convert words or its validity ");
+				
+			exit;
+		}
+		
+		
+		$direction = "";
+		
+		 if ($targetType=="WORD")
+		 {
+		 	$direction= "style='direction:rtl'";
+		 }
+					
+?>
+					
+					<table id='words-pos-context-table' <?=$direction?> >
+						<thead>
+						<tr>
+							<th colspan='<?=($concordanceMaxLevel*2)+1?>'>
+	
+							</th>
+						</tr>
+						<tr>
+							<th>
+								Before
+							</th>
+							<th colspan='1' <?=$direction?> >
+							Concordance for 
+							<?=$targetPOSorWord?>
+							</th>
+							<th>
+								After
+							</th>
+					
+						</tr>
+						</thead>
+						<tbody>
+						 <tr>
+						   <td>
+						   		<ul class='concordance-list'>
+									<?php 
+									foreach ($phrasesBeforeArr as $phrase => $freq)
+									{
+										?>
+										<li>[<?=$freq?>] <?=$phrase?></li>	
+										<?php 
+									}
+									?>
+								</ul>
+						   </td>
+						    <td>
+						    	<table>
+									<?php 
+										$levelArr = $contextArr[$levelName];
+										arsort($levelArr);
+										
+										foreach ($fullSentences as $concordanceIndex => $concSentence)
+										{
+											?>
+											<tr>	
+													<?php
+
+														//$concSentence = markWordWithoutWordIndex($concSentence, $targetPOSorWord, "marked_for_concordance");
+														//echoN($concSentence);
+														
+													$afterPart = mb_substr($concSentence,(mb_strpos($concSentence, $targetPOSorWord)+mb_strlen($targetPOSorWord)));
+													$beforePart = mb_substr($concSentence,0,(mb_strpos($concSentence, $targetPOSorWord)));
+													
+													// to pre-clean for marking code
+													$beforePart = trim($beforePart);
+													$afterPart = trim($afterPart);
+													
+												
+													$afterPart = markSpecificWordInText($afterPart, 0, ".*", "marked_for_concordance_right_left");
+													$beforePart = markSpecificWordInText($beforePart, preg_match_all("/ /",$beforePart), ".*", "marked_for_concordance_right_left");
+													
+												
+													?>
+												<td <?php  if ($targetType=="POS") echo 'align="right"'; else echo 'align="left"';?> >
+													<?=$beforePart?>
+												</td>
+												<td align="center">
+													<span class='marked_for_concordance'><?=$targetPOSorWord ?></span>
+												</td>
+												<td <?php  if ($targetType=="POS") echo 'align="left"'; else echo 'align="right"';?> >
+													<?=$afterPart?>
+												</td>
+											</tr>
+											<?php 
+										}
+									?>
+								</table>
+							</td>
+							<td>
+								<ul class='concordance-list'>
+									<?php 
+									foreach ($phrasesAfterArr as $phrase => $freq)
+									{
+										?>
+										<li>[<?=$freq?>] <?=$phrase?></li>	
+										<?php 
+									}
+									?>
+								</ul>
+							
+							</td>
+						</tr>
+						
+								
+					</tbody>
+					</table>
+
+<?php 
+			
+?>		
+			
+	<?php 
+		require("../footer.php");
+	?>
